60540a5aca18d4b1b7278930bc08451070575663
Dora O <dorozco2390@utexas.edu>
2020-11-14 20:05:07 -0600

    added all functions into get ready function, also added interval function

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function getLocalStorage(key) {
 $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     for (let i = 9; i < 18; i++) {
-    
+
         var row = $(`<div data-time=${i} id='${i}' class="row">`);
 
         var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
-        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
-        
+
         row.append(col1);
         row.append(col2);
         row.append(col3);
@@ -22,28 +22,34 @@ $(document).ready(function () {
         $(".container").append(row);
 
         getLocalStorage(i);
-}})
+    }
 
-function updateColor(){
-    var currentTime = new Date().getHours();
-    for (var i = 9; i < 18; i++) { 
-    console.log(currentTime, $(`#${i}`).data("time"));
-     if ($(`#${i}`).data("time") == currentTime){
-        $(`#text${i}`).addClass( "present");
-    } else if (currentTime < $(`#${i}`).data("time")) {
-        $(`#text${i}`).addClass( "future");
-}}
+    function updateColors() {
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) {
+            console.log(currentTime, $(`#${i}`).data("time"));
+            if ($(`#${i}`).data("time") == currentTime) {
+                $(`#text${i}`).addClass("present");
+            }
+            else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass("future");
+            }
+        }}
 
-function formatAMPM(hours) {
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    return hours + ampm;
-}
-formatAMPM();
+    function formatAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+    formatAMPM();
+
+    setInterval(function () {
+            updateColors();
+        }, 1000);
 
 
-
-
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){})}
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function () { })
+    }
+)
